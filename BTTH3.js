f1539ce1845c8337e88200155cd878ac19720a68
Anh Thu <htathu56@gmail.com>
2026-02-06 22:43:27 +0700

    fix bug Exercise 3

--- BTTH3.js
+++ BTTH3.js
@@ -1,8 +1,30 @@
-let year = 2026;
-let isLeapYear = (year % 4 === 0 && year % 100 != 0);
-console.log( 
-    isLeapYear 
-? "Tháng 2 có 29 ngày"
-: "Tháng 2 có 28 ngày"
-); 
+let month = 2;
+let year = 6;
+
+switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7: 
+    case 8: 
+    case 10:
+    case 12: {
+        console.log(`Tháng ${month} có 31 ngày`);
+        break;
+    }
+    case 4:
+    case 6: 
+    case 9: 
+    case 11: {
+        console.log(`Tháng ${month} có 30 ngày`);
+        break;
+    }
+    case 2:
+    let isLeapYear = (year % 4 === 0 && year % 100 != 0);
+    isLeapYear ? console.log("Tháng 2 có 29 ngày") : console.log("Tháng 2 có 28 ngày");   
+    break;
+    default: 
+        console.log("Tháng không hợp lệ");
+}
+
 
